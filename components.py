7291188a4d45aaf03437ea39f1a83289b7a92b70
lucjalbert <lucjalbert01@gmail.com>
2025-04-14 23:58:12 -0400

    Added cell class and draw methods

--- components.py
+++ components.py
@@ -14,3 +14,60 @@ class Line():
         canvas.create_line(
             self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2
         )
+
+    def __repr__(self):
+        return f"p1 = ({self.start.x}, {self.start.y}) p2 = ({self.end.x}, {self.end.y})"
+
+
+class Cell():
+    def __init__(self, win):
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        
+        self._win = win
+
+
+    def draw(self,  x1, y1, x2, y2, fill_color="black"):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+
+        if self.has_left_wall:
+            l = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(l, fill_color)
+
+        if self.has_top_wall:
+            l = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(l, fill_color)
+
+        if self.has_right_wall:
+            l = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(l, fill_color)
+
+        if self.has_bottom_wall:
+            l = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(l, fill_color)
+
+    
+    def draw_move(self, to_cell, undo=False):
+        if not undo:
+            fill_color = "red"
+        else:
+            fill_color = "gray"
+
+        self_center_x = self._x1 + (self._x2 - self._x1) / 2
+        self_center_y = self._y1 + (self._y2 - self._y1) / 2
+        other_center_x = to_cell._x1 + (to_cell._x2 - to_cell._x1) / 2
+        other_center_y = to_cell._y1 + (to_cell._y2 - to_cell._y1) / 2
+        l = Line(Point(self_center_x, self_center_y), Point(other_center_x, other_center_y))
+        print(l)
+
+        self._win.draw_line(l, fill_color)
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from window import Window
-from components import Point, Line
+from components import Point, Line, Cell
 
 
 def main():
     win = Window(800, 600)
 
-    l = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(l, "black")
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c3 = Cell(win)
+    c3.draw(100, 100, 150, 150)
+
+    c1.draw_move(c2)
+    c2.draw_move(c3, undo=True)
 
     win.wait_for_close()
 
